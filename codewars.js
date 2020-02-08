aaa0dd9cb72da5c30e8f8f263333b776b549f9e8
edom <edom.zhang@gmail.com>
2020-02-08 17:34:21 +1100

    08Feb-2020

--- codewars.js
+++ codewars.js
@@ -192,3 +192,17 @@ function basicOp(operation, value1, value2) {
       return 0;
   }
 }
+
+/**
+ * Q18: Square(n) Sum
+ * Complete the square sum function so that it squares each number passed
+ * into it and then sums the results together.
+ */
+function squareSum(numbers) {
+  let result = 0;
+  for (let i of numbers) {
+    result += i * i;
+  }
+  return result;
+  // return numbers.reduce((sum,num) => sum + (num * num), 0);
+}
--- interview.js
+++ interview.js
@@ -61,3 +61,52 @@ const myHashTable = new HashTable(2);
 myHashTable.set("grapes", 10000);
 myHashTable.set("apples", 9);
 myHashTable.get("apples");
+
+// Q4: 长度为n的数组，数字在0 ~ n-1之间， 找出任意重复的数字。
+// First method: sorted.   Time: O(nlogn) Space:O(1)
+// Second: hash map.       Time: O(n) Space:O(n)
+// Third: change position. Time: O(n) Space:O(1)
+const findSameNum1 = array => {
+  array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] === array[i + 1]) {
+      return array[i];
+    }
+  }
+  return "Not Found";
+};
+
+// console.log(findSameNum1([2, 3, 1, 0, 1]));
+
+const findSameNum2 = array => {
+  let result = {};
+  for (let number of array) {
+    if (result[number]) {
+      return number;
+    } else {
+      result[number] = 1;
+    }
+  }
+  return "Not Found";
+};
+
+// console.log(findSameNum2([2, 3, 1, 0, 1]));
+
+const findSameNum3 = array => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === i) {
+      continue;
+    }
+    if (array[array[i]] === array[i]) {
+      return array[i];
+    } else {
+      let temp = array[i];
+      array[i] = array[array[i]];
+      array[temp] = temp;
+      i--;
+    }
+  }
+  return "Not found";
+};
+
+console.log(findSameNum3([2, 3, 1, 0, 1]));
